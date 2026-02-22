fd8a068e569953556b8243805927a988b5f08d92
luca07giochi-gif <luca07giochi@gmail.com>
2026-02-22 19:35:34 +0100

    Update MainViewController.cs

--- vault.iOS/MainViewController.cs
+++ vault.iOS/MainViewController.cs
@@ -9,20 +9,20 @@ namespace vault.iOS
         {
             base.ViewDidLoad();
 
-            View!.BackgroundColor = UIColor.SystemBackgroundColor;
+            View!.BackgroundColor = UIColor.White;
 
             var titleLabel = new UILabel(new CGRect(24, 90, View.Bounds.Width - 48, 36))
             {
                 Text = "Cassaforte iOS",
                 Font = UIFont.BoldSystemFontOfSize(28),
-                TextColor = UIColor.LabelColor
+                TextColor = UIColor.Black
             };
 
             var statusLabel = new UILabel(new CGRect(24, 140, View.Bounds.Width - 48, 120))
             {
                 Text = "Bootstrap iOS separato da desktop/web.\nProssimo step: apertura vault e navigazione file.",
                 Font = UIFont.SystemFontOfSize(17),
-                TextColor = UIColor.SecondaryLabelColor,
+                TextColor = UIColor.DarkGray,
                 Lines = 0
             };
 
